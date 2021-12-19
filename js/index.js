57e0d56af14996d87c1491814378cc9b1dc8cc3c
Jakub <darker.was@gmail.com>
2021-12-19 15:21:40 +0100

    Moved JS somewhere else

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function div_switch_about(){
+    document.getElementById("about").style.display = "block";
+}
+  function div_switch_main(){
+    document.getElementById("about").style.display = "none";
+}
+
